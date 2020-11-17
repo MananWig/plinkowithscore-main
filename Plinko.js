a1aba810b4fdb8866cb8bd8f0a3686edd27f04da
MananWig <68194162+MananWig@users.noreply.github.com>
2020-11-17 10:39:00 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -1,20 +1,29 @@
-class Plinko{
+class Plinko {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        
+        World.add(world, this.body);
+    }
+    display() {
 
-constructor(x,y){
+        var pos = this.body.position;
+        var angle = this.body.angle;
 
-var options = {
-isStatic: true
-}
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
 
-this.body = Bodies.circle(x,y,10,options);
-this.radius = this.body.circleRadius;
-World.add(world,this.body);
-}
-
-display(){
-var pos = this.body.position;
-fill(0)
-ellipseMode(CENTER)
-ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
-}
-}
+};
